0895700881a9ec7060d83356d9bc15cbf34b96ac
Renan Silva <renanh2008@hotmail.com>
2020-11-12 03:23:28 -0300

    criando a tela home (sem nada) e fazendo as ligações dos botoes login e register

---
+++ app/src/main/java/desafio/modulo2/digitalhousefoods/HomeActivity.kt
@@ -0,0 +1,13 @@
+package desafio.modulo2.digitalhousefoods
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+
+    }
+}
--- app/src/main/java/desafio/modulo2/digitalhousefoods/LoginActivity.kt
+++ app/src/main/java/desafio/modulo2/digitalhousefoods/LoginActivity.kt
@@ -1,12 +1,25 @@
 package desafio.modulo2.digitalhousefoods
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener(openActivity(HomeActivity::class.java))
+        findViewById<Button>(R.id.btnRegister).setOnClickListener(openActivity(HomeActivity::class.java))
+    }
+
+    fun openActivity(activity: Class<out Activity>): View.OnClickListener? {
+        return View.OnClickListener {
+            startActivity(Intent(this, activity))
+        }
     }
 }
