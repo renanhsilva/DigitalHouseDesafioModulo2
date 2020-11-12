bc925ee19abacc2455a92c719c8867504eb13c07
Renan Silva <renanh2008@hotmail.com>
2020-11-12 03:12:14 -0300

    criacao design tela de registro

---
+++ app/src/main/java/desafio/modulo2/digitalhousefoods/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package desafio.modulo2.digitalhousefoods
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
